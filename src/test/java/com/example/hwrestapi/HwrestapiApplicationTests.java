7fd6601e16bc5ab7c9e42a4f3367398042e599af
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-04-04 17:44:29 +0300

    init commit

---
+++ src/test/java/com/example/hwrestapi/HwrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
